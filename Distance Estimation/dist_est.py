f00c1f4df7945456395bf835579ca8bd376c66bc
Rohan Roy Tharakan <rohan.tharakan@gmail.com>
2021-03-13 13:39:10 +0800

    Fixed the confidence compare bug

--- Distance Estimation/dist_est.py
+++ Distance Estimation/dist_est.py
@@ -6,6 +6,7 @@ def conf_init():
     confidence[1.0-(i)]= i * 2
   confidence[0.0] = 0.1
   confidence[1.0] = 1.0
+  confidence[0.01] = 0.0
   confidence = dict(sorted(confidence.items()))
 
   return confidence
@@ -39,6 +40,7 @@ def coordinates(distance,x_dir,y_dir,cur_x_cor,cur_y_cor,x_box):
   if (distance >= 215 ):
     distance -= 215
     conf = distance / 155 
+    conf = round(conf,1)
     if (conf > 0.5):
       conf = 1.0
     img_cor = (cur_x_cor + (2*x_dir), cur_y_cor + (2*y_dir))
@@ -52,17 +54,11 @@ def coordinates(distance,x_dir,y_dir,cur_x_cor,cur_y_cor,x_box):
 
   elif (distance >= 152.5 and distance < 215):
     distance -= 152.5
-    conf = distance / 62.5 
+    conf = distance / 63.5 
+    conf = round(conf,1)
     img_cor = (cur_x_cor + (3*x_dir) , cur_y_cor + (3*y_dir))
 
     CONST = np.array([141.45,263.15,384.85])
-
-    # if (x_box <= CONST_X12):
-    #   idx = 0
-    # elif (x_box > CONST_X12 and x_box <= CONST_X22):
-    #   idx = 1
-    # else:
-    #   idx = 2
     
     idx = find_nearest(CONST,x_box)
     (x_off,y_off) = get_offset(x_dir,y_dir,idx,1)
@@ -71,16 +67,10 @@ def coordinates(distance,x_dir,y_dir,cur_x_cor,cur_y_cor,x_box):
   elif (distance >= 126 and distance < 152.5):
     distance -= 126
     conf = distance / 26.5 
+    conf = round(conf,1)
     img_cor = (cur_x_cor + (4*x_dir) , cur_y_cor + (4*y_dir))
 
     CONST = np.array([69.1,163.6,258.5,352.45,446.95])
-
-    # if (x_box <= CONST_X13):
-    #   idx = 0
-    # elif (x_box > CONST_X13 and x_box <= CONST_X23):
-    #   idx = 1
-    # else:
-    #   idx = 2
 
     idx = find_nearest(CONST,x_box)
     (x_off,y_off) = get_offset(x_dir,y_dir,idx,2)
@@ -89,20 +79,10 @@ def coordinates(distance,x_dir,y_dir,cur_x_cor,cur_y_cor,x_box):
   elif (distance >= 102.5 and distance < 126):
     distance -= 102.5
     conf = distance / 23.5 
+    conf = round(conf,1)
     img_cor = (cur_x_cor + (5*x_dir) , cur_y_cor + (5*y_dir))
 
     CONST = np.array([105.95,181.95,258.25,333.1,409.1])
-
-    # if (x_box <= CONST_X14):
-    #   idx = 0
-    # elif (x_box > CONST_X14 and x_box <= CONST_X24):
-    #   idx = 1
-    # elif (x_box > CONST_X24 and x_box <= CONST_X34):
-    #   idx = 2
-    # elif (x_box > CONST_X34 and x_box <= CONST_X44):
-    #   idx = 3
-    # else:
-    #   idx = 4
 
     idx = find_nearest(CONST,x_box)
     (x_off,y_off) = get_offset(x_dir,y_dir,idx,2)
@@ -111,27 +91,18 @@ def coordinates(distance,x_dir,y_dir,cur_x_cor,cur_y_cor,x_box):
   elif (distance >= 88 and distance < 102.5):
     distance -= 88
     conf = distance / 14.5 
+    conf = round(conf,1)
     img_cor = (cur_x_cor + (6*x_dir) , cur_y_cor + (6*y_dir))
 
     CONST = np.array([59.74,123.7,189.05,252.7,314.8,378.35,442.31])
 
-    # if (x_box <= CONST_X15):
-    #   idx = 0
-    # elif (x_box > CONST_X15 and x_box <= CONST_X25):
-    #   idx = 1
-    # elif (x_box > CONST_X25 and x_box <= CONST_X35):
-    #   idx = 2
-    # elif (x_box > CONST_X35 and x_box <= CONST_X45):
-    #   idx = 3
-    # else:
-    #   idx = 4
     idx = find_nearest(CONST,x_box)
     (x_off,y_off) = get_offset(x_dir,y_dir,idx,3)
     img_cor = (img_cor[0] + x_off, img_cor[1] + y_off)
 
   else:
     print("Distance: " + str(distance) + ". Image too far.")
-    conf = 0.1234567
+    conf = 0.01
     img_cor = (cur_x_cor + (6*x_dir) , cur_y_cor + (6*y_dir))
 
   if (img_cor[0]<0):
@@ -160,17 +131,22 @@ def main(box_len,img_symb,dir,x,y,x_box):
 
   print("Image Coordinates: ",img_cor)
 
-
   conf_lookup = conf_init()
 
-  confidence = round(conf,1)
-  if (conf != 0.1234567):
-    try:
-      for key in symb_confidence[img_symb]:
-        alr_conf = key
-      if (alr_conf<confidence):
-        symb_confidence[img_symb] = {conf_lookup[confidence] : img_cor}
-    except:
-        symb_confidence[img_symb] = {conf_lookup[confidence] : img_cor}
+  # if (conf != 0.1234567):
+  try:
+    for key in symb_confidence[img_symb]:
+      alr_conf = key
+      # print(key)
+    if (alr_conf<conf_lookup[conf]):
+      symb_confidence[img_symb] = {conf_lookup[conf] : img_cor}
+      # print("First ")
+      # print(symb_confidence)
+    else:
+      print("High Confidence value not replaced")
+  except:
+      symb_confidence[img_symb] = {conf_lookup[conf] : img_cor}
+      # print("Second ")
+      # print(symb_confidence)
 
   print(symb_confidence)
